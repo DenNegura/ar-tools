f16d87ff2ccc2b18e7bb081cf144da7611227b15
Denis Negura <den.negura@gmail.com>
2024-03-13 22:06:48 +0200

    commit

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-const loadBtn = document.getElementById("model_load_btn");
+const HIDE_CLASS = 'hide'
 
-loadBtn.addEventListener("click", () => {
-  let input = document.createElement("input");
-  input.type = "file";
+const loadBox = document.getElementById('model_loader');
+const loadBtn = document.getElementById('model_load_btn');
+const infoFile = document.getElementById('model_info');
 
-  input.addEventListener("change", function (event) {
+function setVisible(element, isVisible) {
+  if (isVisible) {
+    element.classList.remove(HIDE_CLASS);
+  } else {
+    element.classList.add(HIDE_CLASS);
+  }
+}
+
+loadBtn.addEventListener('click', () => {
+  let input = document.createElement('input');
+  input.type = 'file';
+
+  input.addEventListener('change', function (event) {
     let file = event.target.files[0];
 
-    console.log("Выбранный файл:", file);
+    setVisible(loadBox, false);
+    setVisible(infoFile, true);
 
-    loadBtn.style.display = 'none';
+    const fileName = document.getElementById('model_name');
+    const fileSize = document.getElementById('model_size');
+
+    fileName.innerHTML = file.name;
+    fileSize.innerHTML = (file.size / 1048576).toFixed(2) + ' MБ';
   });
   
   input.click();
